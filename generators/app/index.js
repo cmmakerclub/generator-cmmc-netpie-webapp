41e7ad3bff068d24fe87ae549cb2f2b10efb0cfa
Nat <nat@cmmc.io>
2017-08-04 15:33:30 +0700

    add basic prompting for an app

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,61 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+'use strict'
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
+const yosay = require('yosay')
 
 module.exports = class extends Generator {
-  prompting() {
+  // note: arguments and options should be defined in the constructor.
+  constructor (args, opts) {
+    super(args, opts)
+
+    // // This makes `appname` argument.
+    this.argument('appname', {type: String, required: false, description: 'app name'})
+    // // And you can then access it later; e.g.
+    this.log(this.options.appname)
+  }
+
+  initializing () {
+    this.props = {}
+    this.pkg = require('../../package.json')
+  }
+
+  prompting () {
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the best ' + chalk.red('generator-cmmc-netpie-webapp') + ' generator!'
-    ));
+    ))
 
     const prompts = [{
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default: this.options.appname || this.appname // Default to current folder name
+    }, {
       type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+      name: 'cool',
+      message: 'Would you like to enable the Cool feature?'
+    }]
 
-    return this.prompt(prompts).then(props => {
+    return this.prompt(prompts).then(answers => {
       // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+      this.log('app name', answers.name)
+      this.log('cool feature', answers.cool)
+      this.props = answers
+    })
   }
 
-  writing() {
+  default () {
+    this.log(`this is default.`)
+  }
+
+  writing () {
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
       this.destinationPath('dummyfile.txt')
-    );
+    )
   }
 
-  install() {
-    this.installDependencies();
+  install () {
+    this.installDependencies()
   }
-};
+}
