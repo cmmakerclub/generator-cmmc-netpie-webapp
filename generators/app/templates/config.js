2dfddc9ef300bcc1d0dd2ef8058b7258e1b903c5
Nat <nat@cmmc.io>
2017-08-04 22:35:17 +0700

    add html template.

--- generators/app/templates/config.js
+++ generators/app/templates/config.js
@@ -1,4 +1,6 @@
-appId = '<%= appId %>'
-appKey = '<%= appKey %>'
-appSecret = '<%= appSecret %>'
-
+var Config = {}
+var Global = {}
+Config.appId = '<%= appId %>'
+Config.appKey = '<%= appKey %>'
+Config.appSecret = '<%= appSecret %>'
+Config.alias = 'DUMMY ALIAS'
--- generators/app/templates/main.js
+++ generators/app/templates/main.js
@@ -1,1 +1,35 @@
-console.log('\'Allo \'Allo!');
+var microgear = window.microgear
+
+microgear = Microgear.create({
+  key: Config.appKey,
+  secret: Config.appSecret,
+})
+
+microgear.on('connected', function () {
+  microgear.setAlias(Config.alias)
+  microgear.subscribe('/gearname/+')
+})
+
+microgear.on('present', function (event) {
+  console.log(event)
+})
+
+microgear.on('absent', function (event) {
+  console.log(event)
+})
+
+microgear.on('message', function (topic, msg) {
+  console.log('on_message', topic, msg)
+  $p = $('<p>' + new Date() + 'message: ' + msg + ' from: ' + topic + '</p>')
+  $('#incoming-messages').prepend($p)
+})
+
+function connect_netpie () {
+  microgear.resettoken(function (err) {
+    if (err) {
+      console.log('reset token err', err)
+    } else {
+      microgear.connect(Config.appId)
+    }
+  })
+}
