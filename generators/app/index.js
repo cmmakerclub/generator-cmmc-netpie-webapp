5082e67bec6ab2361100427c109f21f26e896fe1
Nat <nat@cmmc.io>
2017-08-04 16:12:44 +0700

    Add project files.

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,6 +2,8 @@
 const Generator = require('yeoman-generator')
 const chalk = require('chalk')
 const yosay = require('yosay')
+const mkdirp = require('mkdirp')
+const _s = require('underscore.string')
 
 module.exports = class extends Generator {
   // note: arguments and options should be defined in the constructor.
@@ -49,10 +51,72 @@ module.exports = class extends Generator {
   }
 
   writing () {
+    this._writingGit()
+    this._writingEditorConfig()
+    this._writingMisc()
+    this._writingBower()
+    this._writingScripts()
+    this._writingStyles()
+  }
+
+  _writingStyles () {
+    let css = 'main.css'
+
+    this.fs.copyTpl(
+      this.templatePath(css),
+      this.destinationPath('app/styles/' + css))
+  }
+
+  _writingScripts () {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('main.js'),
+      this.destinationPath('app/scripts/main.js')
     )
+  }
+
+  _writingHtml () {
+
+  }
+
+  _writingMisc () {
+    mkdirp('app/libs')
+    mkdirp('app/images')
+    mkdirp('app/fonts')
+  }
+
+  _writingBower () {
+    const bowerJson = {
+      name: _s.slugify(this.appname),
+      private: true,
+      dependencies: {}
+    }
+
+    bowerJson.dependencies['jquery'] = '~2.1.1'
+    // if (this.includeJQuery) {
+    // }
+
+    this.fs.writeJSON('bower.json', bowerJson)
+    this.fs.copy(
+      this.templatePath('bowerrc'),
+      this.destinationPath('.bowerrc')
+    )
+  }
+
+  _writingEditorConfig () {
+    this.fs.copy(
+      this.templatePath('editorconfig'),
+      this.destinationPath('.editorconfig')
+    )
+  }
+
+  _writingGit () {
+    this.fs.copy(
+      this.templatePath('gitignore'),
+      this.destinationPath('.gitignore'))
+
+    this.fs.copy(
+      this.templatePath('gitattributes'),
+      this.destinationPath('.gitattributes'))
   }
 
   install () {
